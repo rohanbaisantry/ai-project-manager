ca1065ebb26cae3116f19a40ce54ba03d03e10bc
Rohan Baisantry <rohanbaisantry7@gmail.com>
2024-03-20 03:25:20 +0530

    feat: add base for required routes

--- app/chat/routes.py
+++ app/chat/routes.py
@@ -5,6 +5,7 @@ from app.chat.entities import SaveNewChatEntity
 from app.chat.schemas import UserWithChatsSchema
 from app.chat.serializers import serialize_user_with_chats
 from app.chat.use_cases import ChatUseCases
+from app.users.repositories import UserRepository
 
 router = APIRouter(prefix="/chat", tags=["chat"])
 
@@ -16,4 +17,11 @@ async def new_chat_received(
     use_cases = ChatUseCases()
     user = await use_cases.save_new_chat(user_id, data)
     # TODO: Trigger AI and update Tasks + follow-up notifications handling.
+    return serialize_user_with_chats(user)
+
+
+@router.get("/{user_id}")
+async def get_user_chats(user_id: PydanticObjectId) -> UserWithChatsSchema:
+    use_cases = UserRepository()
+    user = await use_cases.get_user_by_id(user_id)
     return serialize_user_with_chats(user)
